4c1133417d704a8b80f1e9a0fe5bf21bd94e269c
zoli456 <zoli456@hotmail.com>
2023-04-10 20:45:05 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,38 +1,42 @@
-import { Message, CommandInteraction, MessageEmbed } from "discord.js"
+import { Message, Interaction, EmbedBuilder } from "discord.js";
 
-declare module "discord-blackjack" {
-    export function Main(message: Message | CommandInteraction, options?: MainOptions): Promise<FinalResult>
+declare function Main(message: Message | Interaction, options?: MainOptions): Promise<FinalResult>;
 
-    interface MainOptions {
-        transition?: "edit" | "delete"
-        buttons?: boolean
-        doubledown?: boolean
-        split?: boolean
-        resultEmbed?: boolean
-        normalEmbed?: boolean
-        normalEmbedContent?: MessageEmbed
-        emojis?: OptionEmojis
-    }
-
-    interface OptionEmojis {
-        clubs: string | "♣️"
-        spades: string | "♠️"
-        hearts: string | "♥️"
-        diamonds: string | "♦️"
-    }
-
-    interface FinalResult {
-        result: string
-        method: string
-        ycard: Array<Card>
-        dcard: Array<Card>
-        message?: Message
-    }
-
-    interface Card {
-        suit: string
-        rank: string
-        value: number
-        emoji: string
-    }
+interface MainOptions
+{
+    transition?: "edit" | "delete";
+    buttons?: boolean;
+    doubledown?: boolean;
+    split?: boolean;
+    resultEmbed?: boolean;
+    normalEmbed?: boolean;
+    normalEmbedContent?: EmbedBuilder;
+    emojis?: OptionEmojis;
 }
+
+interface OptionEmojis
+{
+    clubs: string | "♣️";
+    spades: string | "♠️";
+    hearts: string | "♥️";
+    diamonds: string | "♦️";
+}
+
+interface FinalResult
+{
+    result: string;
+    method: string;
+    ycard: Array<Card>;
+    dcard: Array<Card>;
+    message?: Message;
+}
+
+interface Card
+{
+    suit: string;
+    rank: string;
+    value: number;
+    emoji: string;
+}
+
+export = Main;
